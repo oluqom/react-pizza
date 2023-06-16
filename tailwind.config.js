d3bd74123c994ba1e4c819b0d314c6f4086edcf7
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-06-16 16:42:04 +0700

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "3rem",
+        xl: "4rem",
+      },
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
+};
